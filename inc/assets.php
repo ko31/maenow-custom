1b26a867450ff427deadf3c38818d091dd28649e
Ko Takagi <knockoutmarch@gmail.com>
2019-08-22 21:58:33 +0900

    :wrench: Update assets

--- inc/assets.php
+++ inc/assets.php
@@ -3,11 +3,11 @@
  * Initialize assets
  */
 add_action( 'wp_enqueue_scripts', function () {
-	wp_enqueue_style( 'maenow', plugins_url( 'assets/css/style.css', dirname( __FILE__ ) ), [
+	wp_enqueue_style( 'maenow', plugins_url( 'assets/css/style.min.css', dirname( __FILE__ ) ), [
 		'snow-monkey',
 		'snow-monkey-blocks',
 		'snow-monkey-snow-monkey-blocks'
 	], maenow_version() );
 
-	wp_enqueue_script( 'maenow', plugins_url( 'assets/js/script.js', dirname( __FILE__ ) ), [ 'jquery' ], maenow_version() );
+	wp_enqueue_script( 'maenow', plugins_url( 'assets/js/script.min.js', dirname( __FILE__ ) ), [ 'jquery' ], maenow_version() );
 } );
